f2dbdb06e338aa5efd3d14cc86cf3da1371e3f4b
Fabricio <fa.engeroff1996@gmail.com>
2024-03-13 09:43:16 -0300

    Atualizaçõe na classe ContaDAO e feito algumas configuraçõe na classe ConnectionFactory

--- src/main/java/com/DvFabricio/apiBank/ConnectionFactory.java
+++ src/main/java/com/DvFabricio/apiBank/ConnectionFactory.java
@@ -1,19 +1,28 @@
 package com.DvFabricio.apiBank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-
     public Connection recuperarConexao() {
         try {
-            return DriverManager
-                    .getConnection("jdbc:mysql://localhost:3306/DvFabricio_bank?user=root&password=1234567");
-
+            return createDataSource().getConnection();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("1234567");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
 
--- src/main/java/com/DvFabricio/apiBank/conta/ContaDAO.java
+++ src/main/java/com/DvFabricio/apiBank/conta/ContaDAO.java
@@ -1,11 +1,15 @@
 package com.DvFabricio.apiBank.conta;
 
 import com.DvFabricio.apiBank.domain.Cliente;
+import com.DvFabricio.apiBank.domain.DadosCadastroCliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
@@ -19,11 +23,8 @@ public class ContaDAO {
         var cliente = new Cliente(dadosDaConta.dadosCliente());
         var conta = new Conta(dadosDaConta.numero(), BigDecimal.ZERO, cliente);
 
-
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
                 "VALUES (?, ?, ?, ?, ?)";
-
-
 
         try {
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
@@ -35,8 +36,76 @@ public class ContaDAO {
             preparedStatement.setString(5, dadosDaConta.dadosCliente().email());
 
             preparedStatement.execute();
+            preparedStatement.close();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Set<Conta> listar() {
+        PreparedStatement ps;
+        ResultSet resultSet;
+        Set<Conta> contas = new HashSet<>();
+
+        String sql = "SELECT * FROM conta WHERE esta_ativa = true";
+
+        try {
+            ps = conn.prepareStatement(sql);
+            resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                Integer numero = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                contas.add(new Conta(numero, saldo, cliente));
+            }
+            resultSet.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return contas;
+    }
+
+    public Conta listarPorNumero(Integer numero) {
+        String sql = "SELECT * FROM conta WHERE numero = " + numero + " and esta_ativa = true";
+
+        PreparedStatement ps;
+        ResultSet resultSet;
+        Conta conta = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, numero);
+            resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                Integer numeroRecuperado = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+
+                conta = new Conta(numeroRecuperado, saldo, cliente);
+            }
+            resultSet.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conta;
     }
 }
